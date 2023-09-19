9f71c404f8cc903e2244995a2860599c6284e682
steviss <steviss@gmail.com>
2023-09-19 19:08:50 +0200

    chore(AXD-13): setup prettier, eslinter, docker-compose, env variables, package.json scripts

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['prettier-plugin-astro'],
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import sanity from "@sanity/astro";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+const { SANITY_STUDIO_PROJECT_ID, SANITY_STUDIO_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ""
+);
+
+export const sanityClienfConfig = {
+  projectId: SANITY_STUDIO_PROJECT_ID,
+  dataset: SANITY_STUDIO_DATASET,
+  apiVersion: "2021-03-25",
+  useCdn: true,
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexuix.design",
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [sitemap(), react(), tailwind(), sanity(sanityClienfConfig)],
 });
