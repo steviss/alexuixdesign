69857a04a4f7717e430b51234f230f21478b9072
steviss <steviss@gmail.com>
2023-09-19 19:42:20 +0200

    chore(AXD-13): prettier configuration setup, linted the code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,21 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
-import sanity from "@sanity/astro";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import sanity from '@sanity/astro'
+import { defineConfig } from 'astro/config'
+import { loadEnv } from 'vite'
 
-const { SANITY_STUDIO_PROJECT_ID, SANITY_STUDIO_DATASET } = loadEnv(
-  process.env.NODE_ENV,
-  process.cwd(),
-  ""
-);
+const { SANITY_STUDIO_PROJECT_ID, SANITY_STUDIO_DATASET } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 export const sanityClienfConfig = {
   projectId: SANITY_STUDIO_PROJECT_ID,
   dataset: SANITY_STUDIO_DATASET,
-  apiVersion: "2021-03-25",
+  apiVersion: '2021-03-25',
   useCdn: true,
-};
+}
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alexuix.design",
+  site: 'https://alexuix.design',
   integrations: [sitemap(), react(), tailwind(), sanity(sanityClienfConfig)],
-});
+})
