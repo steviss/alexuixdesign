58dadcf270f1edbf6373e8ae9cc778ad36d96aeb
steviss <steviss@gmail.com>
2023-09-19 21:12:25 +0200

    chore(AXD-12): setup Dockerfile for docker

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,19 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: 'kkapfns5',
-    dataset: 'production'
-  }
-})
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET,
+  },
+  server: {
+    hostname: process.env.SANITY_HOST ?? "0.0.0.0", //so the docker instance routes properly
+    port: parseInt(process.env.SANITY_PORT ?? "3333", 10),
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true, //needs to be enabled if WIN + WSL because of file systems
+      },
+    },
+  },
+});
