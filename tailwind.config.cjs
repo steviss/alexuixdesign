f7ac49922836a7db618e05122de72789763c4f20
miloss98 <stojanovic98m@gmail.com>
2023-09-20 18:26:27 +0200

    chore(AXD-26):fixes to the global configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          '2xl': '1440px',
+        },
+      },
+      colors: {
+        primary: '#161616',
+        secondary: '#C6FB3E',
+        darkGray: '#252525',
+        lightGray: '#2B2B2B',
+        gray: '#E9E9E9',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
