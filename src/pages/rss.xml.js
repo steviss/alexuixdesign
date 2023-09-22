27e839ad353be453647333763f72b314f7cf580b
Stefan Stevic <steviss@gmail.com>
2023-09-22 23:36:14 +0200

    ci(AXD-000): fixes for build

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,14 @@
 import rss from '@astrojs/rss'
-import { useSanityClient } from '@sanity/astro'
-import { convertSanityPostToPost, sanityPostsGroqQuery } from 'src/clients/sanity'
+import { getPosts } from 'src/clients/sanity'
 import { SITE_DESCRIPTION, SITE_TITLE } from 'src/config'
 
 export async function GET(context) {
-  const posts = await useSanityClient().fetch(sanityPostsGroqQuery)
-  const postsTyped = convertSanityPostToPost(posts)
+  const posts = getPosts()
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: postsTyped.map((post) => ({
+    items: posts.map((post) => ({
       ...post.data,
       link: post.slug,
     })),
