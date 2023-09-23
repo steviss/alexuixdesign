26b54a03c906f6a1102033ee98ae984e8f3ed364
Stefan Stevic <steviss@gmail.com>
2023-09-23 10:08:32 +0200

    chore(AXD-29): remove unused code

--- src/clients/sanity/queries/posts/Posts.consts.ts
+++ src/clients/sanity/queries/posts/Posts.consts.ts
@@ -1,7 +1,1 @@
-import type { ConvertSanityPostToPostFunctionRetrieveFieldsI } from './Posts.interface'
-
-export const DEFAULT_RETRIEVE_FIELDS: ConvertSanityPostToPostFunctionRetrieveFieldsI = {
-  body: false,
-}
-
 export const DEFAULT_PREFIX = 'blog'
--- src/clients/sanity/queries/posts/Posts.interface.ts
+++ src/clients/sanity/queries/posts/Posts.interface.ts
@@ -2,10 +2,6 @@ import type { PortableTextProps } from 'astro-portabletext/types'
 
 import type { ConvertSanityDocument } from 'src/types'
 
-export interface ConvertSanityPostToPostFunctionRetrieveFieldsI {
-  body?: boolean
-}
-
 export type ConvertSanityPostToPostFunction = ConvertSanityDocument<SanityPostType, PostType>
 
 export type SanityPostType = {
