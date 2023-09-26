b4bbc549401c3b3086cfddc719d9606f53823c06
Stefan Stevic <steviss@gmail.com>
2023-09-26 10:33:40 +0200

    feat: [AXD-13] added husky, commitlint, jiraprepare, lint-staged configured

--- src/components/react/IconContainer/IconContainer.tsx
+++ src/components/react/IconContainer/IconContainer.tsx
@@ -5,7 +5,7 @@ import type { IconContainerProps } from './IconContainer.interface'
 
 const IconContainer: FC<IconContainerProps> = ({ children, className, ...props }) => {
   return (
-    <div {...props} className={classNames('flex justify-center items-center', className)}>
+    <div {...props} className={classNames('flex items-center justify-center', className)}>
       {children}
     </div>
   )
--- src/components/react/ToolBeltItem/ToolBeltItem.tsx
+++ src/components/react/ToolBeltItem/ToolBeltItem.tsx
@@ -5,7 +5,7 @@ import type { ToolBeltItemProps } from './ToolBeltItem.interface'
 
 const ToolBeltItem: FC<ToolBeltItemProps> = ({ label, className, ...props }) => {
   return (
-    <span {...props} className={classNames('text-2xl md:text-4xl font-nunito font-black text-primary', className)}>
+    <span {...props} className={classNames('font-nunito text-primary text-2xl font-black md:text-4xl', className)}>
       {label}
     </span>
   )
