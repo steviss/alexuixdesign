f2ad515da706e0cd87d84487bb131b4d2421bcde
Stefan Stevic <steviss@gmail.com>
2023-09-28 16:07:16 +0200

    ci: [AXD-000] exclude yaml files from eslint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  '{**/*,*}.{ts,tsx,js,jsx,cjs,astro,yml,yaml}': 'yarn lint',
+  '{**/*,*}.{ts,tsx,js,jsx,cjs,astro}': 'yarn lint',
   '{**/*,*}.{ts,tsx,json,js,jsx,md,astro,cjs,yml,yaml}': 'yarn format',
 }
