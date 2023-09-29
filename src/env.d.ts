cc54063af94a80b3793ee60a3b002ce9c6b8ef38
Stefan Stevic <steviss@gmail.com>
2023-09-29 13:22:33 +0200

    feat: [AXD-12] add enviroment typings, add env.mjs to test for enviromental variables with Zod

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly FONTAWESOME_NPM_AUTH_TOKEN: string
+  readonly SANITY_STUDIO_PROJECT_ID: string
+  readonly SANITY_STUDIO_DATASET: string
+  readonly SANITY_PORT: string
+  readonly SANITY_AUTH_TOKEN: string
+  readonly ASTRO_WEBSITE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
