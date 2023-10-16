8da5bab5593b69d7fd112ceacbbe687ee3094968
steviss <steviss@gmail.com>
2023-10-16 07:54:25 +0200

    refactor: [AXD-000] Refactor Mobile menu and Navigation Header

--- src/components/react/MobileMenu/MobileMenu.tsx
+++ src/components/react/MobileMenu/MobileMenu.tsx
@@ -9,6 +9,7 @@ import type { MobileMenuProps } from './MobileMenu.interface'
 
 const MobileMenu: FC<MobileMenuProps> = ({ children }) => {
   const menuState = useStore(isMenuOpen)
+  const handleToggleMenuState = () => isMenuOpen.set(!menuState)
   return (
     <AnimatePresence>
       {menuState && (
@@ -20,10 +21,9 @@ const MobileMenu: FC<MobileMenuProps> = ({ children }) => {
           transition={{ y: { type: 'spring', stiffness: 300, damping: 30 } }}
           variants={MOBILE_MENU_ANIMATION}
           className="max-w-screen fixed bottom-0 left-0 right-0 z-30 flex max-h-screen flex-col bg-black shadow-md shadow-black md:hidden"
+          onClick={handleToggleMenuState}
         >
-          <nav className="flex flex-col py-4">
-            <ul className="flex list-none flex-col">{children}</ul>
-          </nav>
+          {children}
         </motion.div>
       )}
     </AnimatePresence>
